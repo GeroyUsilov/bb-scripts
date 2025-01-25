355fc684205f419649d9ddf1060857deb7a23837
Christopher Russo <christopherjoel.russo@gmail.com>
2025-01-25 04:33:32 -0600

    Create basic_amber.sh
    add basic amber script

---
+++ basic_amber.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=amberenergy
+#SBATCH --output=energy_calc_%j.log
+#SBATCH --error=energy_calc_%j.err
+#SBATCH --account=pi-amurugan
+#SBATCH --partition=broadwl
+#SBATCH --time=00:45:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+
+
+# The folder to process is passed as an argument
+# The folder to process is passed as an argument
+module load amber
+pdb_file=$1
+
+
+# Create a summary file with timestamp
+date > "energy_summary.txt"
+
+./calc_amber_energy.sh "$pdb_file" 2>&1 | tee -a "energy_summary.txt"
