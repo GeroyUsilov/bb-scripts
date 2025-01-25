4559d54d32cc5047a9972c88fb9acfe46698d60a
Christopher Russo <christopherjoel.russo@gmail.com>
2025-01-25 03:03:33 -0600

    fix file name issues

--- submit_fixbb.sh
+++ submit_fixbb.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=fixedbb_${1}_${2}
-#SBATCH --output=fixedbb_${1}_${2}.out
-#SBATCH --error=fixedbb_${1}_${2}.err
+#SBATCH --job-name=fixedbb
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.err
 #SBATCH --account=pi-amurugan
 #SBATCH --partition=broadwl
 #SBATCH --time=00:04:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 
-
-# Load required modules 
+# Load required modules
 module load rosetta
 
 # Input PDB file and resfile
@@ -22,5 +21,13 @@ if [ -z "$input_pdb" ] || [ -z "$resfile" ]; then
     exit 1
 fi
 
+# Update job name with parameters
+job_name="fixedbb_${input_pdb}_${resfile}"
+scontrol update job $SLURM_JOB_ID name=$job_name
+
 # Run the fixbb script
 ./run_fixbb.sh "$input_pdb" "$resfile"
+
+# Rename output files after job completion
+mv slurm-$SLURM_JOB_ID.out ${job_name}.out
+mv slurm-$SLURM_JOB_ID.err ${job_name}.err
