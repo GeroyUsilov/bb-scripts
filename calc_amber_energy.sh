e52d86b78677a85fa2a7a883666bb970e8dfb1c1
Christopher Russo <christopherjoel.russo@gmail.com>
2025-02-04 01:42:33 -0600

    fix amber dependency issue

--- calc_amber_energy.sh
+++ calc_amber_energy.sh
@@ -8,6 +8,7 @@ error_exit() {
     echo "ERROR: $1" >&2
     exit 1
 }
+export PATH="/software/amber-20-el7-x86_64+intelmpi-2017.up4+intel-17.0/bin:$PATH"
 
 # Function to check if commands exist
 check_dependencies() {
